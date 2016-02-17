2aed96b4ed6a7c2894f68d86579d81f63f6bf935
preethiurs <preethiurs056@gmail.com>
2016-02-17 12:36:59 +0530

    formated the code

--- articleloadereact/App.jsx
+++ articleloadereact/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import LeftPanel from './LeftPanel.jsx';
-import { createStore } from 'redux';
-import {connect,Provider} from 'react-redux';
-
-class  App extends React.Component {
+import React from "react";
+import LeftPanel from "./LeftPanel.jsx";
+import {createStore} from "redux";
+import {connect, Provider} from "react-redux";
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.displayName = '';
     }
+
     render() {
         return <div>
             <h1 id="title">Article Loader</h1>
@@ -19,37 +19,25 @@ class  App extends React.Component {
         </div>;
     }
 }
-
 export default App;
-const loadArticleList = (state=[],action) =>
-{
-
+const loadArticleList = (state = [], action) => {
     switch (action.type) {
         case 'update':
-             return state.map((articles) =>{
-                if(articles.articleId==action.articleId )
-                {
-                    articles.articleTitle=action.articletitle;
-                    articles.articleBody=action.articleBody;
-
+            return state.map((articles) => {
+                if (articles.articleId == action.articleId) {
+                    articles.articleTitle = action.articletitle;
+                    articles.articleBody = action.articleBody;
                 }
                 return articles;
-
-
             });
-
-
         default :
-            let jsonObj=null;
-            let that=this;
+            let jsonObj = null;
+            let that = this;
             let data_file = "./ArtcileList.json";
-            let http_request =  new XMLHttpRequest();
-            http_request.onreadystatechange = function()
-            {
-                if (http_request.readyState == 4)
-                {
+            let http_request = new XMLHttpRequest();
+            http_request.onreadystatechange = function () {
+                if (http_request.readyState == 4) {
                     jsonObj = JSON.parse(http_request.responseText);
-
                 }
             }
             http_request.open("GET", data_file, false);
@@ -57,8 +45,6 @@ const loadArticleList = (state=[],action) =>
             return jsonObj;
     }
 };
-
-
 export const store = createStore(loadArticleList);
 
 
--- articleloadereact/Content.jsx
+++ articleloadereact/Content.jsx
@@ -1,97 +1,90 @@
-import React from 'react';
-import LeftPanel from './LeftPanel.jsx';
+import React from "react";
 class Content extends React.Component {
     constructor(props) {
         super(props);
         this.displayName = '';
-        this.state={articleid : '' , articleTitle : '' , articleBody : '' , contentarea : false ,editarea: false};
-
+        this.state = {articleid: '', articleTitle: '', articleBody: '', contentarea: false, editarea: false};
     }
 
-    editArticle()
-    {
-        this.setState({editarea : true ,contentarea : false});
+    editArticle() {
+        this.setState({editarea: true, contentarea: false});
     }
-    save()
-    {
 
-        var articleId=document.getElementById("articleid").value;
-        var articletitle=document.getElementById("articletitle").value;
-        var articleBody=document.getElementById("articlebody").value;
-
-        this.props.contents.dispatch({type : 'update',articleId : articleId, articletitle : articletitle, articleBody : articleBody});
-        this.setState({editarea : false});
+    save() {
+        var articleId = document.getElementById("articleid").value;
+        var articletitle = document.getElementById("articletitle").value;
+        var articleBody = document.getElementById("articlebody").value;
+        this.props.contents.dispatch({
+            type: 'update',
+            articleId: articleId,
+            articletitle: articletitle,
+            articleBody: articleBody
+        });
+        this.setState({editarea: false});
         alert("Data has been saved successfully");
-
     }
-    cancel()
-    {
+
+    cancel() {
         var r = confirm("You have unsaved changes .. do you want to continue ?? ");
         if (r == true) {
-            this.setState({editarea : false});
+            this.setState({editarea: false});
         }
     }
 
-
-    componentDidMount()
-    {
-        const { store } = this.context;
-        this.unsubscribe= store.subscribe(() => this.forceUpdate());
+    componentDidMount() {
+        const {store} = this.context;
+        this.unsubscribe = store.subscribe(() => this.forceUpdate());
     }
 
-    componentWillUnmount()
-    {
+    componentWillUnmount() {
         this.unsubscribe();
     }
+
     onChangeInput(e) {
-        this.setState({ articleTitle: e.target.value});
-    }
-    onChangeTextArea(e)
-    {
-        this.setState({ articleBody : e.target.value });
+        this.setState({articleTitle: e.target.value});
     }
 
+    onChangeTextArea(e) {
+        this.setState({articleBody: e.target.value});
+    }
 
-    loadArtcile(articleId)
-    {
+    loadArtcile(articleId) {
         const props = this.props;
-        const { store } = this.context;
+        const {store} = this.context;
         const state = store.getState();
-
-        this.setState({contentarea : true , editarea : false});
-
-        var obj=state.map((articles) =>{
-            if(articles.articleId==articleId)
-            {
-
-                this.setState({ articleid : articles.articleId ,articleTitle : articles.articleTitle, articleBody : articles.articleBody , editbtn : true });
+        this.setState({contentarea: true, editarea: false});
+        var obj = state.map((articles) => {
+            if (articles.articleId == articleId) {
+                this.setState({
+                    articleid: articles.articleId,
+                    articleTitle: articles.articleTitle,
+                    articleBody: articles.articleBody,
+                    editbtn: true
+                });
                 return articles;
             }
         })
-
-
     }
+
     render() {
         const props = this.props;
-        const { store } = this.context;
+        const {store} = this.context;
         const state = store.getState();
+        var contentarea, editarea;
+        if (this.state.contentarea) {
+            contentarea = < div id="article">
+                < h2 id="articleTitle">{this.state.articleTitle}</h2>
+                < p id="articleBody">{ this.state.articleBody} </p>
 
-        var contentarea,editarea;
-        if(this.state.contentarea)
-        {
-            contentarea=< div id = "article" >
-                < h2 id = "articleTitle" >{this.state.articleTitle}</h2>
-                    < p id = "articleBody" >{ this.state.articleBody} </p>
-
-                        <Button value="Edit" click={this.editArticle.bind(this)} /></div>;
+                <Button value="Edit" click={this.editArticle.bind(this)}/></div>;
         }
-        if(this.state.editarea)
-        {
-
-            editarea=<div>
+        if (this.state.editarea) {
+            editarea = <div>
                 <input type="hidden" value={this.state.articleid} id="articleid"/>
-                <input type="text" value={this.state.articleTitle} id="articletitle" onChange={this.onChangeInput.bind(this)} /><br/>
-                <textarea rows="4" cols="100" value={this.state.articleBody} onChange={this.onChangeTextArea.bind(this)} id="articlebody"></textarea><br/><br/>
+                <input type="text" value={this.state.articleTitle} id="articletitle"
+                       onChange={this.onChangeInput.bind(this)}/><br/>
+                <textarea rows="4" cols="100" value={this.state.articleBody} onChange={this.onChangeTextArea.bind(this)}
+                          id="articlebody"></textarea><br/><br/>
                 <Button value="Save" click={this.save.bind(this)}/>
                 <Button value="Cancel" click={this.cancel.bind(this)}/>
 
@@ -103,20 +96,19 @@ class Content extends React.Component {
             {editarea}
             <div id="footer">
                 <p> Copyright &copy; Article Loader Inc. </p>
-                    </div>
-            </div>);
-            }
+            </div>
+        </div>);
+    }
+}
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = '';
+    }
 
-            }
-
-            class Button extends React.Component {
-            constructor(props) {
-            super(props);
-            this.displayName = '';
-        }
-            render() {
-            return <button className="button" onClick={this.props.click}>{this.props.value}</button>;
-        }
-        }
-            export default Button;
-            export default Content;
+    render() {
+        return <button className="button" onClick={this.props.click}>{this.props.value}</button>;
+    }
+}
+export default Button;
+export default Content;
