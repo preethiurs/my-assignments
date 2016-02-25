aab1f8a17b2416d84962f52fc5f918733939b148
preethiurs <preethiurs056@gmail.com>
2016-02-25 19:29:28 +0530

    modified the flow

--- DemoProject/Demo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LeftPanel from "./LeftPanel.jsx";
-import App from "./Demo1.jsx";
-import {connect, Provider} from "react-redux";
-import store from "./Demo1.jsx";
-
-console.log("demo jsx");
-console.log("store " ,store);
